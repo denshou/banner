50f1e4f3d353c8d7832f81330981e0d6a6c59de6
denshou <132555762+denshou@users.noreply.github.com>
2023-09-18 03:04:15 +0900

    무한 배너 + 페이지네이션
    이미지 사이즈 확대
    페이지네이션 추가
    맨 앞의 banner6 이미지 제거

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const banner1 = "/banner1.png";
-  const banner2 = "/banner2.png";
+  const banner1 = "/banner1.jpg";
+  const banner2 = "/banner2.jpg";
   const banner3 = "/banner3.jpg";
   const banner4 = "/banner4.jpg";
   const banner5 = "/banner5.jpg";
   const banner6 = "/banner6.jpg";
 
+  const MAX_SLIDES = 6;
+  const TOTAL_SLIDES = MAX_SLIDES + 1;
+
   const swiperRef = useRef();
   const [swiperCurrentPosition, setSwiperCurrentPosition] = useState(0);
   const [loop, setLoop] = useState();
+  const [customInterval, setCustomInterval] = useState(3000);
 
+  const [bannerState, setBannerState] = useState([]);
+
+  const loadBanners = () => {
+    const banners = [
+      banner1,
+      banner2,
+      banner3,
+      banner4,
+      banner5,
+      banner6,
+      banner1,
+    ];
+    setBannerState(banners);
+  };
   useEffect(() => {
+    loadBanners();
+  }, []);
+  console.log(swiperCurrentPosition);
+  useEffect(() => {
+    if (swiperCurrentPosition === 5) {
+      setCustomInterval(500);
+    } else {
+      setCustomInterval(3000);
+    }
     const swiperLoop = setTimeout(() => {
       setSwiperCurrentPosition((prev) => {
-        if (prev < 5) {
+        if (prev < TOTAL_SLIDES - 1) {
           return prev + 1;
         } else return 0;
       });
-    }, 3000);
+    }, customInterval);
 
     setLoop(swiperLoop);
 
@@ -33,21 +60,32 @@ function App() {
       swiperCurrentPosition === 0
         ? `translate(000vw)`
         : `translate(-${swiperCurrentPosition}00vw)`;
+    swiperRef.current.style.transition =
+      swiperCurrentPosition === 0 ? `none` : `0.5s ease-in-out`;
   }, [swiperCurrentPosition]);
 
+  const setIndex = (index) => {
+    setSwiperCurrentPosition(index);
+  };
   return (
     <div className="non-scroll">
-      <div className="container" ref={swiperRef}>
-        <section className="banner">
-          <img src={banner1} alt="" />
-          <img src={banner2} alt="" />
-          <img src={banner3} alt="" />
-          <img src={banner4} alt="" />
-          <img src={banner5} alt="" />
-          <img src={banner6} alt="" />
-          <ul className="slide-pagination"></ul>
+      <div className="container">
+        <section className="banner" ref={swiperRef}>
+          {bannerState.map((src, index) => (
+            <img src={src} alt="" key={index}></img>
+          ))}
         </section>
       </div>
+      <ul className="slide-pagination">
+        <li>
+          <button onClick={() => setIndex(0)}>1</button>
+          <button onClick={() => setIndex(1)}>2</button>
+          <button onClick={() => setIndex(2)}>3</button>
+          <button onClick={() => setIndex(3)}>4</button>
+          <button onClick={() => setIndex(4)}>5</button>
+          <button onClick={() => setIndex(5)}>6</button>
+        </li>
+      </ul>
     </div>
   );
 }
