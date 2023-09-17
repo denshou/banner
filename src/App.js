e491e7d2f1e2fbd42e2dc959a56d9d1e6d6c9031
denshou <132555762+denshou@users.noreply.github.com>
2023-09-18 02:02:45 +0900

    무한반복
    무한반복

--- src/App.js
+++ src/App.js
@@ -9,18 +9,47 @@ function App() {
   const banner5 = "/banner5.jpg";
   const banner6 = "/banner6.jpg";
 
+  const MAX_SLIDES = 6;
+  const TOTAL_SLIDES = MAX_SLIDES + 2;
+
   const swiperRef = useRef();
-  const [swiperCurrentPosition, setSwiperCurrentPosition] = useState(0);
+  const [swiperCurrentPosition, setSwiperCurrentPosition] = useState(1);
   const [loop, setLoop] = useState();
+  const [customInterval, setCustomInterval] = useState(3000);
+  const [isInitial, setIsInitial] = useState(true);
+
+  const [bannerState, setBannerState] = useState([]);
+
+  const loadBanners = () => {
+    const banners = [
+      banner6,
+      banner1,
+      banner2,
+      banner3,
+      banner4,
+      banner5,
+      banner6,
+      banner1,
+    ];
+    setBannerState(banners);
+  };
+  useEffect(() => {
+    loadBanners();
+  }, []);
 
   useEffect(() => {
+    if (swiperCurrentPosition === 6 || swiperCurrentPosition === 0) {
+      setCustomInterval(500);
+    } else {
+      setCustomInterval(3000);
+    }
     const swiperLoop = setTimeout(() => {
       setSwiperCurrentPosition((prev) => {
-        if (prev < 5) {
+        if (prev < TOTAL_SLIDES - 1) {
           return prev + 1;
         } else return 0;
       });
-    }, 3000);
+    }, customInterval);
 
     setLoop(swiperLoop);
 
@@ -29,25 +58,36 @@ function App() {
 
   //스와이퍼 이동
   useEffect(() => {
+    if (isInitial) {
+      swiperRef.current.style.transform = `translate(-${swiperCurrentPosition}00vw)`;
+      swiperRef.current.style.transition = `none`;
+      setIsInitial(false);
+      return;
+    }
     swiperRef.current.style.transform =
       swiperCurrentPosition === 0
-        ? `translate(000vw)`
+        ? `translate(-100vw)`
         : `translate(-${swiperCurrentPosition}00vw)`;
+    swiperRef.current.style.transition =
+      swiperCurrentPosition === 0 ? `none` : `0.5s ease-in-out`;
   }, [swiperCurrentPosition]);
 
+  console.log(swiperCurrentPosition);
   return (
     <div className="non-scroll">
       <div className="container" ref={swiperRef}>
         <section className="banner">
-          <img src={banner1} alt="" />
-          <img src={banner2} alt="" />
-          <img src={banner3} alt="" />
-          <img src={banner4} alt="" />
-          <img src={banner5} alt="" />
-          <img src={banner6} alt="" />
-          <ul className="slide-pagination"></ul>
+          {bannerState.map((src, index) => (
+            <img src={src} alt="" key={index}></img>
+          ))}
         </section>
+        
       </div>
+      <ul className="slide-pagination">
+        <li>
+          <button>1</button>
+        </li>
+      </ul>
     </div>
   );
 }
