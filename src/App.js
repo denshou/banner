25398957788627f5a4b0d81d7894ee582132c5d0
denshou <132555762+denshou@users.noreply.github.com>
2023-09-19 01:23:44 +0900

    feat 배너 완료
    배너 일시정지 버튼
    페이지 확인
    좌우 이동 버튼
    이전, 후 배너 미리 확인
    배너 무한 반복

--- src/App.js
+++ src/App.js
@@ -2,34 +2,41 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const banner1 = "/banner1.png";
-  const banner2 = "/banner2.png";
-  const banner3 = "/banner3.jpg";
-  const banner4 = "/banner4.jpg";
-  const banner5 = "/banner5.jpg";
-  const banner6 = "/banner6.jpg";
+  const banner1 = "/b1.jpg";
+  const banner2 = "/b2.jpg";
+  const banner3 = "/b3.jpg";
+  const banner4 = "/b4.jpg";
+  const banner5 = "/b5.jpg";
+  const banner6 = "/b6.jpg";
+  const play = "/play.svg";
+  const pause = "/pause.svg";
+  const leftArrow = "/left.svg";
+  const rightArrow = "/right.svg";
 
   const MAX_SLIDES = 6;
-  const TOTAL_SLIDES = MAX_SLIDES + 2;
+  const TOTAL_SLIDES = MAX_SLIDES + 4;
 
   const swiperRef = useRef();
-  const [swiperCurrentPosition, setSwiperCurrentPosition] = useState(1);
+  const [swiperCurrentPosition, setSwiperCurrentPosition] = useState(2);
   const [loop, setLoop] = useState();
   const [customInterval, setCustomInterval] = useState(3000);
   const [isInitial, setIsInitial] = useState(true);
+  const [isSwipe, setIsSwipe] = useState(true);
 
   const [bannerState, setBannerState] = useState([]);
 
   const loadBanners = () => {
     const banners = [
-      banner6,
-      banner1,
-      banner2,
-      banner3,
-      banner4,
-      banner5,
-      banner6,
-      banner1,
+      banner5, //0
+      banner6, //1
+      banner1, //2
+      banner2, //3
+      banner3, //4
+      banner4, //5
+      banner5, //6
+      banner6, //7
+      banner1, //8
+      banner2, //9
     ];
     setBannerState(banners);
   };
@@ -38,56 +45,111 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (swiperCurrentPosition === 6 || swiperCurrentPosition === 0) {
+    if (swiperCurrentPosition === 7) {
       setCustomInterval(500);
     } else {
       setCustomInterval(3000);
     }
-    const swiperLoop = setTimeout(() => {
-      setSwiperCurrentPosition((prev) => {
-        if (prev < TOTAL_SLIDES - 1) {
-          return prev + 1;
-        } else return 0;
-      });
-    }, customInterval);
+    if (!isSwipe) {
+      clearTimeout(loop);
+    } else {
+      const swiperLoop = setTimeout(() => {
+        setSwiperCurrentPosition((prev) => {
+          if (prev < 8) {
+            return prev + 1;
+          } else if (swiperCurrentPosition === 8) {
+            return 2;
+          }
+        });
+      }, customInterval);
+      setLoop(swiperLoop);
+    }
 
-    setLoop(swiperLoop);
+    if (swiperCurrentPosition < 2) {
+      setSwiperCurrentPosition(7);
+    }
 
     return clearTimeout(loop);
-  }, [setSwiperCurrentPosition, swiperCurrentPosition]);
+  }, [setSwiperCurrentPosition, swiperCurrentPosition, isSwipe]);
 
   //스와이퍼 이동
   useEffect(() => {
     if (isInitial) {
-      swiperRef.current.style.transform = `translate(-${swiperCurrentPosition}00vw)`;
+      swiperRef.current.style.transform = `translate(-${
+        swiperCurrentPosition * 8
+      }0vw)`;
       swiperRef.current.style.transition = `none`;
       setIsInitial(false);
       return;
     }
     swiperRef.current.style.transform =
-      swiperCurrentPosition === 0
-        ? `translate(-100vw)`
-        : `translate(-${swiperCurrentPosition}00vw)`;
+      swiperCurrentPosition === 8
+        ? `translate(-80vw)`
+        : swiperCurrentPosition === 1
+        ? `translate(-640vw)`
+        : `translate(-${swiperCurrentPosition * 80}vw)`;
     swiperRef.current.style.transition =
-      swiperCurrentPosition === 0 ? `none` : `0.5s ease-in-out`;
+      swiperCurrentPosition === 8 || swiperCurrentPosition < 2
+        ? `none`
+        : `0.5s ease-in-out`;
   }, [swiperCurrentPosition]);
+
+  const handleBannerLeft = (event) => {
+    setCustomInterval(3000);
+    setSwiperCurrentPosition((prev) => prev - 1);
+    event.target.disabled = true;
+    setTimeout(() => {
+      event.target.disabled = false;
+    }, 500);
+
+    console.log(event.target.disabled);
+  };
+  const handleBannerRight = (event) => {
+    setSwiperCurrentPosition((prev) => prev + 1);
+    event.target.disabled = true;
+    setTimeout(() => {
+      event.target.disabled = false;
+    }, 500);
+  };
+  const handleIsSwipe = () => {
+    setIsSwipe((prev) => !prev);
+    console.log(isSwipe);
+  };
+
+  let pagecnt = swiperCurrentPosition - 1;
+  if (swiperCurrentPosition - 1 === 7) pagecnt = 1;
 
   console.log(swiperCurrentPosition);
   return (
     <div className="non-scroll">
-      <div className="container" ref={swiperRef}>
-        <section className="banner">
+      <div className="container">
+        <div className="shade left"></div>
+        <button
+          type="button"
+          className="banner-button button-prev"
+          onClick={handleBannerLeft}
+        >
+          <img src={leftArrow} className="left-arrow" alt="" />
+        </button>
+        <button
+          type="button"
+          className="banner-button button-next"
+          onClick={handleBannerRight}
+        >
+          <img src={rightArrow} className="right-arrow" alt="" />
+        </button>
+        <section className="banner" ref={swiperRef}>
           {bannerState.map((src, index) => (
             <img src={src} alt="" key={index}></img>
           ))}
         </section>
-        
+        <div className="shade right"></div>
+        <button type="button" className="swiper-play" onClick={handleIsSwipe}>
+          {isSwipe && <img src={pause} className="img-pause" alt="" />}
+          {!isSwipe && <img src={play} className="img-play" alt="" />}
+        </button>
+        <div className="pagination">{pagecnt}/6</div>
       </div>
-      <ul className="slide-pagination">
-        <li>
-          <button>1</button>
-        </li>
-      </ul>
     </div>
   );
 }
